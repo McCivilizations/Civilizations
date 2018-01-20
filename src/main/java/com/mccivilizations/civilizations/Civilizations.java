1ef1bac90820fedfca2e1e443dbe78c215a6aeb4
SkySom <skylar.som@gmail.com>
2018-01-19 23:50:17 -0500

    Added BASE

--- src/main/java/com/mccivilizations/civilizations/Civilizations.java
+++ src/main/java/com/mccivilizations/civilizations/Civilizations.java
@@ -3,6 +3,8 @@ package com.mccivilizations.civilizations;
 import com.mccivilizations.civilizations.api.CivilizationsAPI;
 import com.mccivilizations.civilizations.network.reactive.ReactiveNetwork;
 import com.mccivilizations.civilizations.proxy.IProxy;
+import com.teamacronymcoders.base.BaseModFoundation;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
@@ -14,7 +16,7 @@ import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Civilizations.MODID, name = Civilizations.NAME, version = Civilizations.VERSION)
-public class Civilizations {
+public class Civilizations extends BaseModFoundation<Civilizations> {
     public static final String MODID = "civilizations";
     public static final String NAME = "Civilizations";
     public static final String VERSION = "1.0";
@@ -27,20 +29,30 @@ public class Civilizations {
     public static IProxy proxy;
     public static Logger logger;
 
+    public Civilizations() {
+        super(MODID, NAME, VERSION, null);
+    }
+
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        logger = event.getModLog();
         CivilizationsAPI.createInstance(proxy.getCivilizationHandler(), proxy.getLocalizationHandler());
+        super.preInit(event);
+
         ReactiveNetwork.setup(event.getAsmData());
     }
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
-
+        super.init(event);
     }
 
     @EventHandler
     public void postInit(FMLPostInitializationEvent event) {
+        super.postInit(event);
+    }
 
+    @Override
+    public Civilizations getInstance() {
+        return INSTANCE;
     }
 }
